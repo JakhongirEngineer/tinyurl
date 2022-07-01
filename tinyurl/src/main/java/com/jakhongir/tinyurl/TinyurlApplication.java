bb7af53919ef2ea450583cc211706dfffe603110
JakhongirRasulov007 <JakhongirRasulov007@gmail.com>
2022-07-01 14:57:34 +0500

    first commit

---
+++ tinyurl/src/main/java/com/jakhongir/tinyurl/TinyurlApplication.java
@@ -0,0 +1,13 @@
+package com.jakhongir.tinyurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyurlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyurlApplication.class, args);
+    }
+
+}
